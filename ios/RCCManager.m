32adf3f60c89653638fa988eea498d7a04543898
Kupreeva Darina <d.a.kupreeva@tmn2.etagi.com>
2019-02-27 19:33:45 +0500

    fix for 'unable push scene after show/dismiss modal' issue

--- ios/RCCManager.m
+++ ios/RCCManager.m
@@ -106,12 +106,16 @@
     }
     
     id component = nil;
-    
+
     NSMutableDictionary *componentsDic = self.modulesRegistry[componentType];
     if (componentsDic != nil) {
-        component = componentsDic[componentId];
+        if ([componentType isEqualToString:@"NavigationControllerIOS"]) {
+            component = [[componentsDic allValues] lastObject];
+        } else {
+            component = componentsDic[componentId];
+        }
     }
-    
+
     return component;
 }
 
